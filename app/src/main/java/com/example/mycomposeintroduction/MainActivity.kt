46d50b65a53e976766a94ecf3753cccad2670efc
m00nl1ght12 <angelica.magnifico.am96>
2023-03-28 18:06:36 +0200

    My Compose Introduction -> Layout orientation

--- app/src/main/java/com/example/mycomposeintroduction/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeintroduction/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mycomposeintroduction
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.graphics.Color.rgb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -15,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -50,16 +53,52 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ComposeIntroduction(
     modifier: Modifier = Modifier
-) = Surface(color = Color(rgb(110, 1, 82))){
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Header(modifier = Modifier
-            .weight(1f)
-            .wrapContentSize())
-        Footer(modifier = Modifier.wrapContentSize())
-
+) = Surface(color = Color(rgb(110, 1, 82))) {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            Row(modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .weight(.5f)
+                        .fillMaxSize()
+                        .background(color = Color(rgb(110, 1, 82))), Alignment.Center
+                ) {
+                    Header(modifier = Modifier.fillMaxSize())
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(.5f)
+                        .fillMaxSize()
+                        .background(color = Color(rgb(110, 1, 82))), Alignment.Center
+                ) {
+                    Footer(modifier = Modifier.padding(top = 40.dp))
+                }
+            }
+        }
+        else -> {
+            Column(modifier = Modifier) {
+                Box(
+                    modifier = Modifier
+                        .weight(.5f)
+                        .fillMaxSize()
+                        .padding(top = 120.dp)
+                        .background(color = Color((rgb(110, 1, 82)))), Alignment.Center
+                ) {
+                    Header(modifier = Modifier.fillMaxSize())
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(.5f)
+                        .fillMaxSize()
+                        .background(color = Color((rgb(110, 1, 82)))), Alignment.BottomCenter
+                ) {
+                    Footer(modifier = Modifier.padding(bottom = 10.dp))
+                }
+            }
+        }
     }
 }
-
 
 @Composable
 fun Footer(modifier: Modifier = Modifier) {
@@ -79,6 +118,10 @@ fun Footer(modifier: Modifier = Modifier) {
             .height(1.dp)
             .background(color = Color.White))
         FooterRow(text = "angelica.magnifico.am96@gmail.com", image = R.drawable.email_symbol_png_transparent)
+        Divider(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(color = Color.White))
 
         
     }
@@ -95,12 +138,12 @@ fun FooterRow(
             .size(30.dp)
             .padding(end = 3.dp)
             .align(Alignment.CenterVertically), painter = painterResource(id = image) , contentDescription = null, colorFilter = ColorFilter.tint(Color(rgb(215, 197, 233))))
-        Text(text = text, color = Color.White, fontWeight = FontWeight.Light, fontSize = 18.sp, modifier = Modifier.padding(8.dp), fontFamily = font)
+        Text(text = text, color = Color.White, fontWeight = FontWeight.Light, fontSize = 18.sp, modifier = Modifier.padding(8.dp), fontFamily = secondFont)
         
     }
 }
 
-val appFontFamily = FontFamily(
+val firstFont = FontFamily(
         Font(
             resId = R.font.dazzlingdivas,
             weight = FontWeight.Light,
@@ -108,7 +151,7 @@ val appFontFamily = FontFamily(
         )
     )
 
-val font = FontFamily(
+val secondFont = FontFamily(
     Font(
         resId = R.font.littlesunshine,
         weight = FontWeight.ExtraBold,
@@ -121,22 +164,68 @@ val font = FontFamily(
 @Composable
 fun Header(
     modifier: Modifier = Modifier
-) = Column(modifier = modifier.padding(top= 130.dp, bottom = 150.dp)) {
+) = Column(modifier = modifier, Arrangement.Center) {
     Image(painter = painterResource(id = R.drawable.livello_0), contentDescription = null, modifier = Modifier
         .size(150.dp)
-        .padding(5.dp)
         .align(Alignment.CenterHorizontally))
-    Text(text = "Angelica Magnifico", color = Color.White, modifier = modifier.align(Alignment.CenterHorizontally).padding(top = 15.dp), fontSize = 30.sp, fontFamily = appFontFamily)
-    Text(text = "Aspiring Junior Android Developer", color = Color(rgb(215, 197, 233)), modifier = modifier
-        .align(Alignment.CenterHorizontally),
-    fontFamily = font, fontSize = 18.sp)
-
-
+    Text(text = "Angelica Magnifico", color = Color.White, modifier = Modifier
+        .align(Alignment.CenterHorizontally)
+        .padding(top = 12.dp), fontSize = 30.sp, fontFamily = firstFont)
+    Text(text = "Aspiring Junior Android Developer", color = Color(rgb(215, 197, 233)), modifier = Modifier
+        .align(Alignment.CenterHorizontally)
+        .padding(top = 3.dp),
+    fontFamily = secondFont, fontSize = 18.sp)
 }
 
 
-@Preview(showBackground = true)
+//@SuppressLint("SwitchIntDef")
+@Preview
+@Preview()
 @Composable
-fun DefaultPreview() = Column(modifier = Modifier.fillMaxSize()){
-    ComposeIntroduction(modifier = Modifier)
+fun DefaultPreview()
+    = Surface(color = Color(rgb(110, 1, 82))) {
+        val configuration = LocalConfiguration.current
+        when (configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                Row(modifier = Modifier.fillMaxSize()) {
+                    Box(
+                        modifier = Modifier
+                            .weight(.5f)
+                            .fillMaxSize()
+                            .background(color = Color(rgb(110, 1, 82))), Alignment.Center
+                    ) {
+                        Header(modifier = Modifier.fillMaxSize())
+                    }
+                    Box(
+                        modifier = Modifier
+                            .weight(.5f)
+                            .fillMaxSize()
+                            .background(color = Color(rgb(110, 1, 82))), Alignment.Center
+                    ) {
+                        Footer(modifier = Modifier.padding(top = 40.dp))
+                    }
+                }
+            }
+            else -> {
+                Column(modifier = Modifier) {
+                    Box(
+                        modifier = Modifier
+                            .weight(.5f)
+                            .fillMaxSize()
+                            .padding(top = 120.dp)
+                            .background(color = Color((rgb(110, 1, 82)))), Alignment.Center
+                    ) {
+                        Header(modifier = Modifier.fillMaxSize())
+                    }
+                    Box(
+                        modifier = Modifier
+                            .weight(.5f)
+                            .fillMaxSize()
+                            .background(color = Color((rgb(110, 1, 82)))), Alignment.BottomCenter
+                    ) {
+                        Footer(modifier = Modifier.padding(bottom = 10.dp))
+                    }
+                }
+            }
+        }
     }
