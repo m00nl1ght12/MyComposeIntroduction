709784ad303b25ffa8129ded82b25df12f8e11c2
roberto@ux310uq <robertox92@gmail.com>
2023-05-17 12:05:03 +0200

    My Compose Introduction -> About Me Screen update

--- app/src/main/java/com/example/mycomposeintroduction/AboutMeScreen.kt
+++ app/src/main/java/com/example/mycomposeintroduction/AboutMeScreen.kt
@@ -4,47 +4,58 @@ package com.example.mycomposeintroduction
 import android.graphics.Color.rgb
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.w3c.dom.Text
-
 @Preview
 @Composable
 fun AboutMeScreen (
     modifier: Modifier = Modifier
 ) = Surface(color = (Color(rgb(110, 1, 82)))){
-    Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Row(modifier = Modifier
-            .weight(.1f)
-            .fillMaxSize()) {
-            AboutMe(modifier = Modifier.fillMaxSize())
-            }
-        Row(modifier = Modifier
-            .weight(.4f)
-            .fillMaxSize()) {
-            AboutMeDescription(modifier = Modifier.fillMaxSize())
+    Column (
+        modifier = Modifier
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            AboutMe(modifier = Modifier.fillMaxWidth())
         }
-        Row(modifier = Modifier
-            .weight(.1f)
-            .wrapContentSize()) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            AboutMeDescription(modifier = Modifier.fillMaxWidth())
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            SoftSkills()
+        }
+        Row (modifier = Modifier.fillMaxWidth()) {
+            SoftSkillsDescription()
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
             HardSkills(modifier = Modifier)
         }
-        Row(modifier = Modifier
-            .weight(.4f)
-            .fillMaxSize()) {
-            HardSkillsDescription(modifier = Modifier.fillMaxSize())
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            HardSkillsDescription()
         }
+
     }
 }
 
@@ -52,25 +63,15 @@ fun AboutMeScreen (
 
 
 @Composable
-fun AboutMeDescription (modifier: Modifier) {
+fun AboutMeDescription (modifier: Modifier = Modifier) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Sono una appassionata di tecnologia, \n" +
-                    "ho di recente intrapreso un percorso di studio \n" +
-                    "sulla programmazione ed i suoi linguaggi.\n" +
-                    "Ho partecipato ad eventi come il GDG DevFest,\n" +
-                    "il Bootcamp Puglia Women Code,\n" +
-                    "dove ho avuto modo di osservare più da vicino \n" +
-                    "questo mondo, conoscendo e ascoltando gli sviluppatori\n" +
-                    "degli ambiti più disparati. \n" +
-                    "Sono convinta di voler intraprendere un percorso\n" +
-                    "nell’IT vista la mia creatività e la mia voglia \n" +
-                    "di conoscere ancora più a fondo un mondo \n" +
-                    "sempre in evoluzione.\n",
+        Text(text = stringResource(id = R.string.about_me_description),
             color = Color.White,
             fontWeight = FontWeight.Light,
             fontSize = 20.sp,
             modifier = Modifier
-                .padding(top = 8.dp)
+                .padding(horizontal = 28.dp)
+                .padding(top = 18.dp)
                 .align(Alignment.CenterHorizontally),
             fontFamily = secondFont
         )
@@ -79,14 +80,14 @@ fun AboutMeDescription (modifier: Modifier) {
 }
     
 @Composable
-fun AboutMe(modifier: Modifier){
+fun AboutMe(modifier: Modifier = Modifier){
     Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
-        Row(modifier = Modifier
-            .fillMaxSize()
-            .wrapContentHeight()
-            .padding(top = 13.dp)
-            .align(Alignment.CenterHorizontally)
-            .background(Color.White.copy(alpha = 0.5F)),
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp)
+                .align(Alignment.CenterHorizontally)
+                .background(Color.White.copy(alpha = 0.5F)),
         ) {
             Text(
                 text = stringResource(id = R.string.chi_sono_id),
@@ -104,71 +105,71 @@ fun AboutMe(modifier: Modifier){
 }
 
 @Composable
-fun HardSkillsDescription(modifier: Modifier) {
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+fun SoftSkillsDescription(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 28.dp)
+            .padding(top = 18.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
         Text(
-            text = "• Conoscenza base di Kotlin, Java, JetPack Compose, HTML, CSS",
+            text = stringResource(id = R.string.lavoro_di_squadra),
             color = Color.White,
             fontWeight = FontWeight.Light,
             fontSize = 20.sp,
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .align(Alignment.CenterHorizontally),
             fontFamily = secondFont
         )
-        Spacer(
-            modifier = Modifier.height(10.dp)
-        )
         Text(
-            text = "• Microsoft Word, Power Point",
+            text = stringResource(id = R.string.pensiero_analitico),
             color = Color.White,
             fontWeight = FontWeight.Light,
             fontSize = 20.sp,
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .align(Alignment.CenterHorizontally),
             fontFamily = secondFont
         )
-        Spacer(
-            modifier = Modifier.height(10.dp)
-        )
         Text(
-            text = "• Suite Adobe (Photoshop, InDesign, Bridge, Premiere)",
+            text = stringResource(id = R.string.gestione_tempo),
             color = Color.White,
             fontWeight = FontWeight.Light,
             fontSize = 20.sp,
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .align(Alignment.CenterHorizontally),
             fontFamily = secondFont
         )
-        Spacer(
-            modifier = Modifier.height(10.dp)
-        )
         Text(
-            text = "• Editing foto e video",
+            text = stringResource(id = R.string.motivazione),
             color = Color.White,
             fontWeight = FontWeight.Light,
             fontSize = 20.sp,
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .align(Alignment.CenterHorizontally),
             fontFamily = secondFont
         )
+        Text(
+            text = stringResource(id = R.string.organizzazione),
+            color = Color.White,
+            fontWeight = FontWeight.Light,
+            fontSize = 20.sp,
+            fontFamily = secondFont
+        )
+        Text(
+            text = stringResource(id = R.string.strumenti_informatici),
+            color = Color.White,
+            fontWeight = FontWeight.Light,
+            fontSize = 20.sp,
+            fontFamily = secondFont
+        )
+
     }
 }
 
 @Composable
-fun HardSkills(modifier: Modifier) {
+fun SoftSkills(modifier: Modifier = Modifier) {
     Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
-        Row(modifier = Modifier
-            .fillMaxSize()
-            .wrapContentHeight()
-            .align(Alignment.CenterHorizontally)
-            .background(Color.White.copy(alpha = 0.5F)),
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 18.dp)
+                .align(Alignment.CenterHorizontally)
+                .background(Color.White.copy(alpha = 0.5F)),
         ) {
             Text(
-                text = "HARD SKILLS",
+                text = stringResource(id = R.string.soft_skills_label),
                 color = Color.White,
                 fontWeight = FontWeight.Light,
                 fontSize = 70.sp,
@@ -178,6 +179,74 @@ fun HardSkills(modifier: Modifier) {
                 fontFamily = secondFont
             )
         }
-
     }
 }
+
+
+        @Composable
+        fun HardSkillsDescription(modifier: Modifier = Modifier) {
+            Column(
+                modifier = modifier
+                    .padding(horizontal = 28.dp)
+                    .padding(top = 18.dp, bottom = 30.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Text(
+                    text = "• Conoscenza base di Kotlin, Java, JetPack Compose, HTML, CSS",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 20.sp,
+                    fontFamily = secondFont
+                )
+                Text(
+                    text = "• Microsoft Word, Power Point",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 20.sp,
+                    fontFamily = secondFont
+                )
+                Text(
+                    text = "• Suite Adobe (Photoshop, InDesign, Bridge, Premiere)",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 20.sp,
+                    fontFamily = secondFont
+                )
+                Text(
+                    text = "• Editing foto e video",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 20.sp,
+                    fontFamily = secondFont
+                )
+
+            }
+        }
+
+
+        @Composable
+        fun HardSkills(modifier: Modifier) {
+            Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 18.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .background(Color.White.copy(alpha = 0.5F)),
+                ) {
+                    Text(
+                        text = "HARD SKILLS",
+                        color = Color.White,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 70.sp,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterVertically)
+                            .padding(start = 28.dp),
+                        fontFamily = secondFont
+                    )
+                }
+
+
+        }
+    }
+
